f4cb890b56a4d9ded5c76b8022f36ce95cb80113
NastyaMittseva <nastya_mittseva@mail.ru>
2020-03-29 21:40:49 +0500

    add MathTest

---
+++ app/src/test/java/com/example/lesson3_homework/MathTest.kt
@@ -0,0 +1,11 @@
+package com.example.lesson3_homework
+
+import org.junit.Assert
+import org.junit.Test
+
+class MathTest {
+    @Test
+    fun addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2)
+    }
+}
